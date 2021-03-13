1fc4bf2ff6d708d489505c56ff62e1e7242c25c2
qikaile <2649019047@qq.com>
2021-03-13 17:51:02 +0800

    first commit

---
+++ src/pages/Main.vue
@@ -0,0 +1,141 @@
+<!-- @format -->
+
+<template>
+  <div class="index-page" v-loading="isLoading">
+    <HeaderNav />
+    <div id="vditor" class="vditor" />
+  </div>
+</template>
+
+<script>
+import Vditor from 'vditor'
+import HeaderNav from './partials/HeaderNav'
+import defaultText from '@config/default'
+
+export default {
+  name: 'index-page',
+
+  data() {
+    return {
+      isLoading: true,
+      isMobile: window.innerWidth <= 960,
+      vditor: null
+    }
+  },
+
+  created() {
+    this.setDefaultText()
+  },
+
+  components: {
+    HeaderNav
+  },
+
+  mounted() {
+    this.initVditor()
+    this.$nextTick(() => {
+      this.isLoading = false
+    })
+  },
+
+  methods: {
+    initVditor() {
+      const that = this
+      const options = {
+        width: this.isMobile ? '100%' : '80%',
+        height: '0',
+        tab: '\t',
+        counter: '999999',
+        typewriterMode: true,
+        mode: 'sv',
+        preview: {
+          delay: 100,
+          show: !this.isMobile
+        },
+        outline: true,
+        upload: {
+          max: 5 * 1024 * 1024,
+          // linkToImgUrl: 'https://sm.ms/api/upload',
+          handler(file) {
+            let formData = new FormData()
+            for (let i in file) {
+              formData.append('smfile', file[i])
+            }
+            let request = new XMLHttpRequest()
+            request.open('POST', 'https://sm.ms/api/upload')
+            request.onload = that.onloadCallback
+            request.send(formData)
+          }
+        }
+      }
+      this.vditor = new Vditor('vditor', options)
+      this.vditor.focus()
+    },
+    onloadCallback(oEvent) {
+      const currentTarget = oEvent.currentTarget
+      if (currentTarget.status !== 200) {
+        return this.$message({
+          type: 'error',
+          message: currentTarget.status + ' ' + currentTarget.statusText
+        })
+      }
+      let resp = JSON.parse(currentTarget.response)
+      let imgMdStr = ''
+      if (resp.code === 'invalid_source') {
+        return this.$message({
+          type: 'error',
+          message: resp.message
+        })
+      }
+      if (resp.code === 'image_repeated') {
+        imgMdStr = `![](${resp.images})`
+      } else if (resp.code === 'success' || resp.success) {
+        imgMdStr = `![${resp.data.filename}](${resp.data.url})`
+      }
+      this.vditor.insertValue(imgMdStr)
+    },
+    setDefaultText() {
+      const savedMdContent = localStorage.getItem('vditorvditor') || ''
+      if (!savedMdContent.trim()) {
+        localStorage.setItem('vditorvditor', defaultText)
+      }
+    }
+  }
+}
+</script>
+
+<style lang="less">
+@import './../assets/styles/style.less';
+.index-page {
+  width: 100%;
+  height: 100%;
+  background-color: @white;
+  .flex-box-center(column);
+  .vditor {
+    position: absolute;
+    top: @header-height;
+    max-width: @max-body-width;
+    width: 80%;
+    height: calc(100vh - 100px);
+    margin: 20px auto;
+    text-align: left;
+  }
+  .vditor-reset {
+    font-size: 14px;
+  }
+  .vditor-textarea {
+    font-size: 14px;
+    height: 100% !important;
+  }
+}
+
+@media (max-width: 960px) {
+  .index-page {
+    .vditor {
+      height: calc(100vh - 60px);
+      padding: auto 10px;
+      margin: 0px auto;
+    }
+  }
+}
+</style>
